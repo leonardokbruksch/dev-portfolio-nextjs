19a083cc532281cb42292f0ab7abd4ee171aacdf
leonardo <leonardokbruksch@hotmail.com>
2025-09-16 17:35:57 +0800

    test: remove background

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Background from "@/components/ui/background";
+// import Background from "@/components/ui/background";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +34,7 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange>
-          <Background />
+          {/* <Background /> */}
           {children}
         </ThemeProvider>
       </body>
